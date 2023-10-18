7517925f5b47dbd5301b6b1f9e8d75b9758681ae
Shim, hyeongjun <rhrlqnf@gmail.com>
2023-10-18 10:37:34 +0900

    two

--- src/f201914072/Main.java
+++ src/f201914072/Main.java
@@ -3,6 +3,7 @@ package f201914072;
 public class Main {
 
 	public static void main(String[] args) {
+		Two.print();
 		System.out.println("one 201914072");
 	}
 }
---
+++ src/f201914072/Two.java
@@ -0,0 +1,8 @@
+package f201914072;
+
+public class Two {
+
+	public static void print() {
+        System.out.println("two");
+    }
+}
